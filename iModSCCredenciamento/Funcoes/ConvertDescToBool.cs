84cf28f7ab0293b43cc4ea8ae087a553562879af
Grupo Estrela <engenharia@grupoestrela.com>
2018-11-13 11:01:56 -0300

    Comentario Test01

--- iModSCCredenciamento/Funcoes/ConvertDescToBool.cs
+++ iModSCCredenciamento/Funcoes/ConvertDescToBool.cs
@@ -9,7 +9,7 @@ using System.Windows.Data;
 namespace iModSCCredenciamento.Funcoes
 {
     public class ConvertDescToBool : IValueConverter
-    {
+    { 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
 
