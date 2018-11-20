ebdcde742341522102a50846cd2205bd3c83a87b
Grupo Estrela <engenharia@grupoestrela.com>
2018-11-20 17:25:00 -0300

    Projetos de classes renomeados

--- Infra/Properties/Settings.Designer.cs
+++ Infra/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Infra.Properties {
+namespace IMOD.Infra.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
