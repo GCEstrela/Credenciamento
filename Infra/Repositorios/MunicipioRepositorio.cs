3c4a5872ed7c4ad96b96b2340a5097188f1358aa
Renato Máximo da Silva Júnior <renatomaximo@grupoestrela.com>
2018-11-23 13:19:50 -0300

    Terceira alteração Repositorios

--- Infra/Repositorios/MunicipioRepositorio.cs
+++ Infra/Repositorios/MunicipioRepositorio.cs
@@ -35,37 +35,6 @@ namespace IMOD.Infra.Repositorios
         #region Metodos
 
         /// <summary>
-        ///     Criar registro
-        /// </summary>
-        /// <param name="entity"></param>
-        public void Criar(Municipio entity)
-        {
-             using (var conn = _dataBase.CreateOpenConnection())
-            {
-                using (var cmd = _dataBase.InsertText ("Municipio", conn))
-                {
-                    try
-                    {
-                        //cmd.Parameters.Add (_dataBase.CreateParameter (new ParamInsert ("ColaboradorAnexoId", entity.ColaboradorAnexoId, true)));
-                        //cmd.Parameters.Add (_dataBase.CreateParameter (new ParamInsert ("Descricao", entity.Descricao, false)));
-                        //cmd.Parameters.Add (_dataBase.CreateParameter (new ParamInsert ("NomeArquivo", entity.NomeArquivo, false)));
-                        //cmd.Parameters.Add (_dataBase.CreateParameter (new ParamInsert ("ColaboradorID", entity.ColaboradorId, false)));
-                        //cmd.Parameters.Add (_dataBase.CreateParameter (new ParamInsert ("Arquivo", entity.Arquivo, false)));
-
-                        var key = Convert.ToInt32 (cmd.ExecuteScalar());
-
-                        entity.MunicipioId = key;
-                    }
-                    catch (Exception ex)
-                    {
-                        Utils.TraceException (ex);
-                        throw;
-                    }
-                }
-            }
-        }
-
-        /// <summary>
         ///     Buscar pela chave primaria
         /// </summary>
         /// <param name="id"></param>
@@ -74,7 +43,7 @@ namespace IMOD.Infra.Repositorios
         {
            using (var conn = _dataBase.CreateOpenConnection())
             {
-                using (var cmd = _dataBase.SelectText ("Municipio", conn))
+                using (var cmd = _dataBase.SelectText ("Municipios", conn))
 
                 {
                     try
@@ -103,13 +72,14 @@ namespace IMOD.Infra.Repositorios
         {
             using (var conn = _dataBase.CreateOpenConnection())
             {
-                using (var cmd = _dataBase.SelectText ("Municipio", conn))
+                using (var cmd = _dataBase.SelectText ("Municipios", conn))
 
                 {
                     try
                     {
                         //cmd.Parameters.Add (_dataBase.CreateParameter (new ParamSelect ("NomeArquivo", o, 0).Like()));
                         //cmd.Parameters.Add (_dataBase.CreateParameter (new ParamSelect ("ColaboradorID", o, 1).Igual()));
+                        cmd.Parameters.Add(_dataBase.CreateParameter(new ParamSelect("Nome", objects, 0).Igual()));
 
                         var reader = cmd.ExecuteReaderSelect();
                         var d1 = reader.MapToList<Municipio>();
@@ -125,60 +95,6 @@ namespace IMOD.Infra.Repositorios
             }
         }
 
-        /// <summary>
-        ///     Alterar registro
-        /// </summary>
-        /// <param name="entity"></param>
-        public void Alterar(Municipio entity)
-        {
-            using (var conn = _dataBase.CreateOpenConnection())
-            {
-                using (var cmd = _dataBase.UpdateText ("Municipio", conn))
-                {
-                    try
-                    {
-                        cmd.Parameters.Add (_dataBase.CreateParameter (new ParamUpdate ("MunicipioId", entity.MunicipioId, true)));
-                        //cmd.Parameters.Add (_dataBase.CreateParameter (new ParamUpdate ("Descricao", entity.Descricao, false)));
-                        //cmd.Parameters.Add (_dataBase.CreateParameter (new ParamUpdate ("NomeArquivo", entity.NomeArquivo, false)));
-                        //cmd.Parameters.Add (_dataBase.CreateParameter (new ParamUpdate ("ColaboradorID", entity.ColaboradorId, false)));
-                        //cmd.Parameters.Add (_dataBase.CreateParameter (new ParamUpdate ("Arquivo", entity.Arquivo, false)));
-
-                        cmd.ExecuteNonQuery();
-                    }
-                    catch (Exception ex)
-                    {
-                        Utils.TraceException (ex);
-                        throw;
-                    }
-                }
-            }
-        }
-
-        /// <summary>
-        ///     Deletar registro
-        /// </summary>
-        /// <param name="predicate"></param>
-        public void Remover(Municipio entity)
-        {
-            using (var conn = _dataBase.CreateOpenConnection())
-            {
-                using (var cmd = _dataBase.DeleteText ("Municipio", conn))
-                {
-                    try
-                    {
-                        cmd.Parameters.Add (_dataBase.CreateParameter (new ParamDelete ("MunicipioId", entity.MunicipioId).Igual()));
-
-                        cmd.ExecuteNonQuery();
-                    }
-                    catch (Exception ex)
-                    {
-                        Utils.TraceException (ex);
-                    }
-                }
-            }
-        }
-
-        
         #endregion
     }
 }
