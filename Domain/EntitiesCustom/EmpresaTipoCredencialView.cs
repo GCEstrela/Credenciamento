6e1d1aec5b2dfb5db6b2cffbe3e6d30e2af496c3
Renato Máximo da Silva Júnior <renatomaximo@grupoestrela.com>
2019-01-10 10:39:26 -0300

    Sync com Release02 (Dev04)

---
+++ Domain/EntitiesCustom/EmpresaTipoCredencialView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMOD.Domain.EntitiesCustom
+{
+    public class EmpresaTipoCredencialView
+    {
+
+        #region  Propriedades
+
+        public int ColaboradorCredencialId { get; set; }
+        public int ColaboradorEmpresaId { get; set; }
+        public int TipoCredencialId { get; set; }
+        public bool Ativa { get; set; }
+        public int EmpresaId { get; set; }
+        public int Colaborador { get; set; }
+        public string Descricao { get; set; }
+
+        #endregion
+
+    }
+}
