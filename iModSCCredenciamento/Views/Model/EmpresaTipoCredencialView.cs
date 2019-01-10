be0e404383dcd01bc93c7fd76d5f9c250c866bce
Mihainuan De Sá Silva <mihainuan@gmail.com>
2019-01-10 09:31:32 -0300

    Developer02 sync with Release02

---
+++ iModSCCredenciamento/Views/Model/EmpresaTipoCredencialView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iModSCCredenciamento.Views.Model
+{
+
+    public class EmpresaTipoCredencialView
+
+    {
+
+        #region  Propriedades
+
+        public int ColaboradorCredencialId { get; set; }
+        public int ColaboradorEmpresaId { get; set; }
+        public int TipoCredencialId { get; set; }
+        public bool Ativa { get; set; }
+        public int EmpresaId { get; set; }
+        public int Colaborador { get; set; }
+        public string Descricao { get; set; }
+
+        #endregion
+
+    }
+}
