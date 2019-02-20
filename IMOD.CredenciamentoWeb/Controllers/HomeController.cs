d271119be6aa5569d7aeb98680975ebb58adff04
GCEstrela <engenharia@grupoestrela.com>
2019-02-20 11:43:20 -0300

    - Limpeza - Correcoes

--- IMOD.CredenciamentoWeb/Controllers/HomeController.cs
+++ IMOD.CredenciamentoWeb/Controllers/HomeController.cs
@@ -1,31 +1,17 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
 namespace IMOD.CredenciamentoWeb.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Login()
-        {
-            return View("Login");
-        }
-
+        // GET: Home
         public ActionResult Index()
         {
             return View();
         }
-
-
-        //public ActionResult Grid()
-        //{
-        //    return View("Grid");
-        //}
-
-        //public ActionResult Tabela()
-        //{
-        //    return View("Tables");
-        //}
-
-
-
     }
 }
