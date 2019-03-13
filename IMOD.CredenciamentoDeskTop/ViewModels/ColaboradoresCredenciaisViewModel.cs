36cab6f79a544cdafa69f8aca237e32575771dd7
adelmojose <adelmojps@hotmail.com>
2019-03-13 10:54:08 -0300

    Correção de erro ao adiciona novas credenciais colaborador veiculo

--- IMOD.CredenciamentoDeskTop/ViewModels/ColaboradoresCredenciaisViewModel.cs
+++ IMOD.CredenciamentoDeskTop/ViewModels/ColaboradoresCredenciaisViewModel.cs
@@ -116,6 +116,7 @@ namespace IMOD.CredenciamentoDeskTop.ViewModels
         {
             MensagemAlerta = string.Empty;
             if (entity == null) return;
+            if (entity.ColaboradorCredencialId <= 0) return;
 
             #region Habilitar botão de impressao e mensagem ao usuario
 
