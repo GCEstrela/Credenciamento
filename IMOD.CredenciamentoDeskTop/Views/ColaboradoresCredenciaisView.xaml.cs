2e2dc588d84b0d78eb65c3110ac739a2d2a6a942
adelmojose <adelmojps@hotmail.com>
2019-03-14 15:37:33 -0300

    ajuste check devolução

--- IMOD.CredenciamentoDeskTop/Views/ColaboradoresCredenciaisView.xaml.cs
+++ IMOD.CredenciamentoDeskTop/Views/ColaboradoresCredenciaisView.xaml.cs
@@ -98,6 +98,14 @@ namespace IMOD.CredenciamentoDeskTop.Views
         {
             
             btnImprimirCredencial.IsEnabled = _viewModel.HabilitaImpressao;
+
+            if ((CredencialStatus)cmbCredencialStatus.SelectedItem != null)
+            {
+                _viewModel.HabilitaCheckDevolucao(((CredencialStatus)cmbCredencialStatus.SelectedItem).CredencialStatusId, 0);
+                chkDevolucaoMotivo.IsChecked = _viewModel.IsCheckDevolucao;
+                chkDevolucaoMotivo.Visibility = _viewModel.VisibilityCheckDevolucao;
+                chkDevolucaoMotivo.Content = _viewModel.TextCheckDevolucao;
+            }
         }
 
         private void CmbMotivacao_SelectionChanged(object sender, SelectionChangedEventArgs e)
