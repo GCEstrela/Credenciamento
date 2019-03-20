e5fec2173a4796e39a13c108f197661ec989a075
adelmojose <adelmojps@hotmail.com>
2019-03-20 10:17:43 -0300

    bloqueio da empresa e colaborador para credenciais (colaborador e veiculo) com pendência de devolução

--- IMOD.Application/Service/ColaboradorCredencialService.cs
+++ IMOD.Application/Service/ColaboradorCredencialService.cs
@@ -145,7 +145,7 @@ namespace IMOD.Application.Service
                //Criar um pendenci impeditiva ao constatar o motivo da credencial
                var pendImp = CredencialMotivo.BuscarPelaChave (entity.CredencialMotivoId);
             if (pendImp == null) throw new InvalidOperationException ("Não foi possível obter a entidade credencial motivo");
-            var impeditivo = pendImp.Impeditivo;
+            var impeditivo = pendImp.Impeditivo & entity.DevolucaoEntregaBoId == 0;
             if (!impeditivo) return;
             //Criar uma pendencia impeditiva,caso sua natureza seja impeditiva
 
