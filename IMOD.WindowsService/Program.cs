805e8d68135bfaaaa68a80baf48abcc3c6a80ad1
Marcos <mfsouzaa@gmail.com>
2019-04-09 11:03:39 -0300

    Inclusão do projeto do serviço

---
+++ IMOD.WindowsService/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IMOD.WindowsService
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        static void Main()
+        {
+#if (!DEBUG)
+            {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[]
+                {
+                new Service1()
+                };
+                ServiceBase.Run(ServicesToRun);
+            }
+#else
+
+            Service1 service = new Service1();
+            Timer _timer;
+
+            // Chamada do seu método para Debug.
+            //service.MetodoRealizaFuncao(null);
+            _timer = new Timer(service.MetodoRealizaFuncao, null, 0, 30000);
+            System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+#endif
+        }
+    }
+    
+}
---
+++ IMOD.WindowsService/Service1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using IMOD.Application.Service;
+using IMOD.Application.Interfaces;
+using System.Threading;
+
+namespace IMOD.WindowsService
+{
+    public partial class Service1 : ServiceBase
+    {
+
+        private IEmpresaContratosService _service = new EmpresaContratoService();
+        private Timer _timer;
+
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _timer = new Timer(MetodoRealizaFuncao, null, 0, 120000);            
+        }
+
+        internal void MetodoRealizaFuncao(object state)
+        {
+            try
+            {
+                CriarLog("serviço rodando:" + DateTime.Now);
+                //var empresaContratos = _service.Listar().Where(ec => ec.StatusId == 0).ToList();
+                var empresaContratos = _service.Listar().ToList();
+                empresaContratos.ForEach(ec =>
+                    {
+                        string texto = ec.Descricao + ((ec.Validade < DateTime.Now) ? " Vencido em : " : " Válido até : ") + ec.Validade;                        
+                        CriarLog(string.Format("Contrato: {0}" , texto));
+                        Console.WriteLine(string.Format("Contrato: {0}", texto));
+                    }
+                );
+            }
+            catch (Exception ex)
+            {
+                CriarLog(ex.Message);
+            }
+        }
+
+        protected override void OnStop()
+        {
+            CriarLog("Finalizado");
+        }
+
+        private void CriarLog(object state)
+        {
+            StreamWriter vWriter = new StreamWriter(@"C:\Tmp\Logs\log.txt", true);            
+            vWriter.WriteLine(state.ToString());
+            vWriter.Flush();
+            vWriter.Close();
+        }
+    }
+}
