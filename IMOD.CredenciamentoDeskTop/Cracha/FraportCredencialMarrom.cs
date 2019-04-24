c5b105cfae1b6da367f9bc2d1541af65d1b8e8e9
adelmojose <adelmojps@hotmail.com>
2019-04-24 11:03:12 -0300

    layouts Fraport Porto Alegre com a imagem do QRCode

--- IMOD.CredenciamentoDeskTop/Cracha/FraportCredencialMarrom.cs
+++ IMOD.CredenciamentoDeskTop/Cracha/FraportCredencialMarrom.cs
@@ -95,6 +95,14 @@ namespace IMOD.CredenciamentoDeskTop.Cracha {
                 return this.ReportDefinition.Sections[5];
             }
         }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.Shared.IParameterField Parameter_PathImgQrCode {
+            get {
+                return this.DataDefinition.ParameterFields[0];
+            }
+        }
     }
     
     [System.Drawing.ToolboxBitmapAttribute(typeof(CrystalDecisions.Shared.ExportOptions), "report.bmp")]
