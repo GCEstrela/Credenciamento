22510b15081bb23b5ea40e49f2f0e3d9f34f2eff
adelmojose <adelmojps@hotmail.com>
2019-04-26 15:57:59 -0300

    viewmodel web

---
+++ IMOD.PreCredenciamentoWeb/Models/AutorizacaoViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IMOD.PreCredenciamentoWeb.Models
+{
+    public class AutorizacaoViewModel
+    {
+
+        public int VeiculoCredencialId { get; set; }
+        public string EmpresaNome { get; set; } 
+        public string Descricao { get; set; }
+        public DateTime Emissao { get; set; }
+        public DateTime Validade { get; set; }
+        public string PlacaIdentificador { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Cor { get; set; }
+        public string Identificacao1 { get; set; }
+        public string Identificacao2 { get; set; }
+        public string Categoria { get; set; }
+        public string Frota { get; set; }
+
+    }
+}
---
+++ IMOD.PreCredenciamentoWeb/Models/CredencialViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IMOD.PreCredenciamentoWeb.Models
+{
+    public class CredencialViewModel
+    {
+        public int ColaboradorCredencialID { get; set; }
+        public string Colete { get; set; }
+        public DateTime Emissao { get; set; }
+        public DateTime Validade { get; set; }
+        public string Matricula { get; set; }
+        public string Cargo { get; set; }
+        public string EmpresaNome { get; set; }
+        public string EmpresaApelido { get; set; }
+        public string CNPJ { get; set; }
+        public string Sigla { get; set; }
+        public string Logo { get; set; }
+        public string ColaboradorNome { get; set; }
+        public string ColaboradorApelido { get; set; }
+        public string CPF { get; set; }
+        public string RG { get; set; }
+        public string RGOrgLocal { get; set; }
+        public string RGOrgUF { get; set; }
+        public string TelefoneEmergencia { get; set; }
+        public string Foto { get; set; }
+        public byte[] Foto2 { get; set; }
+        public byte[] Logo2 { get; set; }
+        public string Identificacao1 { get; set; }
+        public string Identificacao2 { get; set; }
+        public string CNHCategoria { get; set; }
+        public string LayoutRPT { get; set; }
+        public string CrachaCursos { get; set; }
+        public string ImpressaoMotivo { get; set; }
+        public string TerceirizadaNome { get; set; }
+        public string RNE { get; set; }
+        public string Passaporte { get; set; }
+        public DateTime PassaporteValidade { get; set; }
+        public bool ManuseioBagagem { get; set; }
+        public bool Motorista { get; set; }
+        public string Cnh { get; set; }
+        public bool OperadorPonteEmbarque { get; set; }
+        public bool FlagCcam { get; set; }
+
+    }
+}
