a4c2970021b45c3016da691bfd7d45db06402379
adelmojose <adelmojps@hotmail.com>
2019-05-07 11:52:02 -0300

    criação de classe constante; ajuste data validade credencial

---
+++ IMOD.CredenciamentoDeskTop/Constantes/Constantes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMOD.CredenciamentoDeskTop.Constantes
+{
+    static class Constantes
+    {
+        public const int diasPorAno = 365;
+        public const int diasPorMes = 30;
+        
+    }
+}
