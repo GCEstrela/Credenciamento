70f00b37b0865a645d3db6b7d68ad80eb5a1bc76
Renato Máximo da Silva Júnior <renatomaximo@grupoestrela.com>
2019-06-10 14:32:55 -0300

    Ajustes ( App.config )

--- IMOD.Infra/Properties/Settings.Designer.cs
+++ IMOD.Infra/Properties/Settings.Designer.cs
@@ -26,9 +26,9 @@ namespace IMOD.Infra.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=172.16.190.108\\SQLEXPRESS;Initial Catalog=D_iModCredenciamento;User I" +
-            "D=imod;Password=imod;Min Pool Size=5;Max Pool Size=15;Connection Reset=True;Conn" +
-            "ection Lifetime=600;Trusted_Connection=no;MultipleActiveResultSets=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=GCTEC65\\SQLEXPRESS;Initial Catalog=D_iModCredenciamento;User ID=imod;" +
+            "Password=imod;Min Pool Size=5;Max Pool Size=15;Connection Reset=True;Connection " +
+            "Lifetime=600;Trusted_Connection=no;MultipleActiveResultSets=True")]
         public string Credenciamento {
             get {
                 return ((string)(this["Credenciamento"]));
