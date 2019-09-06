75230aebff303c6bdf2986cc4d9a1d049accb57c
Renato Máximo da Silva Júnior <renatomaximo@grupoestrela.com>
2019-09-06 10:34:58 -0300

    CardHolder PopUp

---
+++ IMOD.CredenciamentoDeskTop/Windows/PopUp.xaml.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tree
+{
+    /// <summary>
+    /// Interaction logic for PopUpPesquisaEmpresa.xaml
+    /// </summary>
+    public partial class PopUp : Window
+    {
+        public PopUp()
+        {
+            InitializeComponent();
+            MouseDown += Window_MouseDown;
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+
+
+
+        private void Procurar_bt_Click(object sender, RoutedEventArgs e)
+        {
+            ICollection<Guid> guids = TCHG.SelectedItems;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
