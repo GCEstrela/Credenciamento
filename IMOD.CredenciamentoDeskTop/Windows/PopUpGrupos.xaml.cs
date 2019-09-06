7e6463d7ae8816d73c65af05e590e011d53dd35d
Marcos <mfsouzaa@gmail.com>
2019-09-06 11:10:46 -0300

    popup Grupos

---
+++ IMOD.CredenciamentoDeskTop/Windows/PopUpGrupos.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IMOD.CredenciamentoDeskTop.Windows
+{
+    /// <summary>
+    /// Interação lógica para PopUpGrupos.xam
+    /// </summary>
+    public partial class PopUpGrupos : UserControl
+    {
+        public PopUpGrupos()
+        {
+            InitializeComponent();
+        }
+    }
+}
