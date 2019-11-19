0ea324e1fb4d17194ad147fc28aa06d2f0717f1c
Renato Máximo da Silva Júnior <renatomaximo@grupoestrela.com>
2019-11-19 09:27:49 -0300

    Ajustes ( EmpresaSeguros - Arquivo anexo )

--- IMOD.CredenciamentoDeskTop/Views/EmpresasSegurosView.xaml.cs
+++ IMOD.CredenciamentoDeskTop/Views/EmpresasSegurosView.xaml.cs
@@ -62,5 +62,40 @@ namespace IMOD.CredenciamentoDeskTop.Views
                 throw ex;
             }
         }
+
+        private void BtnBuscarArquivo_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //var filtro = "Imagem files (*.pdf)|*.pdf|All Files (*.*)|*.*";
+                //var arq = WpfHelp.UpLoadArquivoDialog (filtro, 700);
+                var filtro = "Imagem files (*.pdf)|*.pdf";
+                var arq = WpfHelp.UpLoadArquivoDialog(filtro, _viewModel.IsTamanhoArquivo);
+                if (arq == null) return;
+                _viewModel.Entity.Arquivo = arq.FormatoBase64;
+                _viewModel.Entity.NomeArquivo = arq.Nome;
+                txtApoliceArquivo.Text = arq.Nome;
+            }
+            catch (Exception ex)
+            {
+                WpfHelp.Mbox(ex.Message);
+                Utils.TraceException(ex);
+            }
+        }
+
+        private void AbrirArquivo_bt_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _viewModel.BuscarAnexo(_viewModel.Entity.EmpresaSeguroId);
+                var arquivoStr = _viewModel.Entity.Arquivo;
+                var arrBytes = Convert.FromBase64String(arquivoStr);
+                WpfHelp.AbrirArquivoPdf(_viewModel.Entity.NomeArquivo, arrBytes);
+            }
+            catch (Exception ex)
+            {
+                Utils.TraceException(ex);
+            }
+        }
     }
 }
