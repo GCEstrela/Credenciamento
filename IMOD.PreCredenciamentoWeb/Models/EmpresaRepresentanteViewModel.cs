5e445eeab661e69b606fb394ff448cb94f14df9b
Marcos <mfsouzaa@gmail.com>
2020-02-05 10:05:27 -0300

    incluindo classe de empresa representante

---
+++ IMOD.PreCredenciamentoWeb/Models/EmpresaRepresentanteViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IMOD.PreCredenciamentoWeb.Models
+{
+    public class EmpresaRepresentanteViewModel
+    {
+        #region  Propriedades
+
+        public int EmpresaSignatarioId { get; set; }
+        public int EmpresaId { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Celular { get; set; }
+        public bool Principal { get; set; }
+        public string Assinatura { get; set; }
+        public string NomeArquivo { get; set; }
+        public string RG { get; set; }
+        public string OrgaoExp { get; set; }
+        public string RGUF { get; set; }
+        public int TipoRepresentanteId { get; set; }
+        #endregion
+    }
+}
