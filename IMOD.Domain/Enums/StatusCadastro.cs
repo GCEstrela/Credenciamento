0bbcbce5088a829aec0d1c4b0ec115decd923cc5
Marcos <mfsouzaa@gmail.com>
2020-02-05 13:07:27 -0300

    Enum Status cadastro

--- IMOD.Domain/Enums/StatusCadastro.cs
+++ IMOD.Domain/Enums/StatusCadastro.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,14 @@ namespace IMOD.Domain.Enums
     ///     Tipo de pendencia
     /// </summary>
     public enum StatusCadastro
-    {        
+    {
+        [Description("Aguardando Aprovação Inclusão")]
         AGUARDANDO_APROVACAO_INCLUSAO,
+        [Description("Aguardando Revisçao")]
         AGUARDANDO_REVISAO,
+        [Description("Aguardando Aprovação Revisão")]
         AGUARDANDO_APROVACAO_REVISAO,
+        [Description("Aprovado")]
         APROVADO
     }
 
