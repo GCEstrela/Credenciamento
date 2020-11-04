1a139a656c306bb2cf745af5aafb3ad9094b5d97
Otavio Pereira <desenvolvimento2@grupoestrela.com>
2020-11-04 08:13:08 -0300

    Cadastro Web - Criação Enum para aba de observação tipo de situação

---
+++ IMOD.Domain/Enums/TipoSituacao.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMOD.Domain.Enums
+{
+    /// <summary>
+    ///     Tipo de situação
+    /// </summary>
+    public enum TipoSituacao
+    {        
+        [Description("1ª Emissão")]
+        PRIMEIRA_EMISSAO,
+        [Description("Danificado")]
+        DANIFICADO,
+        [Description("Alteração de Acesso")]
+        ALTERACAO_ACESSO,
+        [Description("Autorização para Dirigir")]
+        AUTORIZACAO_DIRIGIR,
+        [Description("Renovação")]
+        RENOVACAO,
+        [Description("Extraviado")]
+        EXTRAVIADO,
+        [Description("Alteração de Função")]
+        ALTERACAO_FUNCAO,
+        [Description("Credencial Temporária")]
+        CREDENCIAL_TEMPORARIA,
+        [Description("Outros")]
+        OUTROS
+    }
+}
