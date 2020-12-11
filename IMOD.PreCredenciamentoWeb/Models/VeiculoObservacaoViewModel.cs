60bd3b51a8ffbcbd89561661d4b1e17114164ac1
Otavio Pereira <desenvolvimento2@grupoestrela.com>
2020-12-11 08:15:46 -0300

    Cadastro Web - Implementação das ViewMoldes de Anexo e Itens de Revisão

---
+++ IMOD.PreCredenciamentoWeb/Models/VeiculoObservacaoViewModel.cs
@@ -0,0 +1,31 @@
+﻿using IMOD.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IMOD.PreCredenciamentoWeb.Models
+{
+    public class VeiculoObservacaoViewModel
+    {
+        public int VeiculoObservacaoId { get; set; }
+        public int VeiculoId { get; set; }
+        [Display(Name = "Observação")]
+        public string Observacao { get; set; }
+        public bool Impeditivo { get; set; }
+        public bool Resolvido { get; set; }
+        public int UsuarioRevisao { get; set; }
+        public DateTime DataRevisao { get; set; }
+        public int? VeiculoObservacaoRespostaId { get; set; }
+        public string ObservacaoResposta { get; set; }
+
+        public string UsuarioRevisaoInfo 
+        {   get 
+            {                
+                return Funcoes.GetDescription((UsuarioRevisao)UsuarioRevisao);
+            } 
+        }
+    }
+}
